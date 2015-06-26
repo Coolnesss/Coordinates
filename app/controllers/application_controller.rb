52ffc0ca3aa90bb2b14c0ab1b1397c2ce3076498
Chang <chang.rajani@gmail.com>
2015-06-26 18:51:00 +0300

    Minor fixes. Add colors to causes in report list.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  helper_method :current_user
+  helper_method :current_user, :cause_color
 
   def current_user
     return nil if session[:user_id].nil? or User.all.find_by(id:session[:user_id]).nil?
@@ -12,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def ensure_that_signed_in
     redirect_to signin_path, notice:'You should be signed in to view reports' if current_user.nil?
+  end
+
+  def cause_color(cause)
+    colors = {"fixed" => "rgb(157, 136, 255)", "spam" => "rgb(157, 136, 255)", "other" => "rgb(243, 255, 136)"}
+    colors[cause.downcase]
   end
 end
