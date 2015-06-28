d070558ea52a24c6b3ee60f46ef7309433b94b96
Chang <chang.rajani@gmail.com>
2015-06-28 19:29:01 +0300

    trying to make deploy work

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
+set :use_sudo,        true #i changed this
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
