51c75ec495889cec437b81abfdc6b6e9a29af2da
Chang <chang.rajani@gmail.com>
2015-06-29 13:05:04 +0300

    Trying to get env variables to work

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,10 @@ set :application,     'Coordinates'
 set :user,            'chang'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+set :default_env, {
+  "SECRET_KEY_BASE" => ENV["SECRET_KEY_BASE"]
+}
+
 
 # Don't change these unless you know what you're doing
 set :pty,             true
