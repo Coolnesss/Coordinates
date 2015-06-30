55bf31e9dbd3b4f88b4d956b388bc726ea9c373a
Chang <chang.rajani@gmail.com>
2015-06-30 13:35:22 +0300

    trying to make env variables work

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,16 @@ module Coordinates
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
     config.middleware.use Rack::JSONP
     config.middleware.use Rack::Cors do
       allow do
