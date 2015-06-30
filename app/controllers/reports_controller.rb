112124ba5c57b323de072e6b161b640b602fedba
Chang <chang.rajani@gmail.com>
2015-06-30 12:36:49 +0300

    Add ignored to allowed params

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -75,6 +75,6 @@ class ReportsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def report_params
-      params.require(:report).permit(:email, :description, :cause, :position_id)
+      params.require(:report).permit(:email, :description, :cause, :position_id, :ignored)
     end
 end
