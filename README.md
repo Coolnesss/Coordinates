1a2d1a3b01be5ab0f42e691c36abc612dcc4cbc0
Chang <chang.rajani@gmail.com>
2015-07-06 15:53:53 +0300

    Minor improvements. Update README.

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 [![security](https://hakiri.io/github/Coolnesss/Coordinates/master.svg)](https://hakiri.io/github/Coolnesss/Coordinates/master)
 [![Build Status](https://travis-ci.org/Coolnesss/Coordinates.svg)](https://travis-ci.org/Coolnesss/Coordinates)
-Backend JSON API for http://ronil.kapsi.fi/F
+#### Backend, JSON API for http://fillari.info
+Backend site [here](http://128.199.45.65/)
+This application serves as a database for position and report data.
+
+* Written in Ruby on Rails, database Postgresql
+* Paperclip, Amazon S3 for pictures
+* Capistrano, Puma, nginx
+* Rspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,10 +14,19 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require "paperclip/matchers"
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/spec/test_files/"])
+  end
+
+  config.include Paperclip::Shoulda::Matchers
+
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
