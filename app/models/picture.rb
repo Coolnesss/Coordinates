ba2732822632b27dc6aed5465a642d741cec4380
Chang <chang.rajani@gmail.com>
2015-07-07 23:27:20 +0300

    Remove useless medium size

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,8 +3,9 @@ class Picture < ActiveRecord::Base
   belongs_to :position
 
   has_attached_file :image,
+    :preserve_files => false,
     source_file_options: { all: '-auto-orient' },
-    :styles => {:original => "2000x2000>", :medium => "300x300>", :thumb => "85x85>" },
+    :styles => {:original => "2000x2000>", :thumb => "85x85>" },
     :default_url => "null"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
